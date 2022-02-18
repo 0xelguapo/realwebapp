adce8b7d8cb2057deca06809e11a1841736bc007
Eric <84822642+erichen114@users.noreply.github.com>
2022-02-17 16:54:48 -0800

    route, icon, navbar

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,27 @@ import { AuthContextProvider } from "../shared/context/auth-context";
 import "../styles/globals.css";
 import Amplify from "aws-amplify";
 import awsconfig from "../aws-exports";
+import nProgress from "nprogress";
+import { useRouter } from "next/router";
+import { useEffect, useCallback } from "react";
 
 Amplify.configure(awsconfig);
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => nProgress.start());
+    router.events.on("routeChangeComplete", () => nProgress.done());
+    router.events.on("routeChangeError", () => nProgress.done());
+
+    return () => {
+      router.events.off("routeChangeStart", () => nProgress.start());
+      router.events.off("routeChangeComplete", () => nProgress.done());
+      router.events.off("routeChangeError", () => nProgress.done());
+    };
+  }, [router]);
+
   return (
     <AuthContextProvider>
       <Component {...pageProps} />
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,21 @@
-import { useEffect } from "react"
-import { useRouter } from "next/router"
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import styles from "../styles/Dashboard.module.css";
 
 export default function Dashboard() {
-  const router = useRouter()
-  
+  const router = useRouter();
+
   return (
-    <div>hi</div>
-  )
+    <div className={styles.pageContainer}>
+      <Head>
+        <title>CoAgent Dashboard</title>
+        <meta
+          name="description"
+          content="Client Management for Real Estate and Insurance Agents"
+        />
+        <link rel="icon" href="/icon.svg" />
+      </Head>
+    </div>
+  );
 }
