5f0be35d25b70ab6286e2e000812508c49ff4554
Eric <84822642+erichen114@users.noreply.github.com>
2022-02-22 20:49:46 -0800

    Add Modal Add Client

--- shared/components/AddClient/AddClient.js
+++ shared/components/AddClient/AddClient.js
@@ -1,22 +1,90 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./AddClient.module.css";
 import Modal from "../UI/Modal/Modal.js";
 import Image from "next/image";
+import Input from "../Input.js";
+import useForm from "../../hooks/form-hook.js";
+import { VALIDATOR_REQUIRE } from "../../utility/validators";
 
 export default function AddClient() {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [formState, inputHandler] = useForm(
+    {
+      name: {
+        value: "",
+        isValid: false,
+      },
+      company: {
+        value: "",
+        isValid: true,
+      },
+      phone: {
+        value: "",
+        isValid: true,
+      },
+      email: {
+        value: "",
+        isValid: true,
+      },
+    },
+    false
+  );
 
-  const handleClick = () => {
-    setOpen(!open)
-  }
+  const clientDetails = {
+    name: formState.inputs.name.value,
+    company: formState.inputs.company.value,
+    phone: formState.inputs.phone.value,
+    email: formState.inputs.email.value,
+  };
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+  }, [open]);
 
   return (
     <div className={styles.container}>
-      <button onClick={handleClick} className={styles.button}>
+      <button onClick={handleOpen} className={styles.button}>
         <Image src="/add.svg" width={15} height={15} alt="add client" />
         Add Client
       </button>
-      <Modal open={open}/>
+      {open && (
+        <Modal onOpen={handleOpen}>
+          <div className={styles.modalContainer}>
+            <h2>Add a Contact</h2>
+            <form>
+              <div className={styles.inputBlock}>
+                <Input
+                  id="name"
+                  onInput={inputHandler}
+                  headerText="Name"
+                  placeholder="Required"
+                  errorText="Please enter a name"
+                  validators={[VALIDATOR_REQUIRE()]}
+                />
+                <Input
+                  id="company"
+                  onInput={inputHandler}
+                  headerText="Company"
+                />
+              </div>
+              <Input
+                id="phone"
+                onInput={inputHandler}
+                headerText="Phone Number"
+              />
+              <Input id="email" onInput={inputHandler} headerText="Email" />
+            </form>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 }
