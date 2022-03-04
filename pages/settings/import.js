d377002ac558b710b41ee36dafbf56bc98739bb9
Eric <84822642+erichen114@users.noreply.github.com>
2022-03-04 12:20:00 -0800

    fix style

--- pages/settings/import.js
+++ pages/settings/import.js
@@ -1,29 +1,56 @@
 import { useState, useCallback } from "react";
 import Head from "next/head";
+import Image from "next/image";
 import DashboardLayout from "../../shared/components/UI/Layouts/DashboardLayout";
 import { useDropzone } from "react-dropzone";
 
 function Import() {
   const [currentStep, setCurrentStep] = useState(0);
-  const onDrop = useCallback((acceptedFiles) => {
-    //do something with files
+  const [selectedFile, setSelectedFile] = useState();
+
+  const onDrop = useCallback((acceptedFile) => {
+    setSelectedFile(acceptedFile);
+    console.log(acceptedFile);
   }, []);
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    multiple: false,
+  });
 
   const steps = [
     {
       Component: (
         <div className="container h-full flex flex-col py-24 px-12 justify-center">
-          <div className="flex justify-center items-center border basis-1/5" {...getRootProps()}>
+          <div
+            className="flex justify-center items-center border-2 border-dashed border-lightgray-500 border-radius basis-1/5"
+            {...getRootProps()}
+          >
             <input {...getInputProps()} />
             {isDragActive ? (
               <p>Drop your file here!</p>
             ) : (
-              <p className="">Drag and drop your .csv file here</p>
+              <>
+                {selectedFile ? (
+                  <div className="flex items-center">
+                    <Image
+                      src="/fileSelected.svg"
+                      width={40}
+                      height={40}
+                      alt="file"
+                    />
+                    <p className="text-lg font-bold">{selectedFile[0].name}</p>
+                  </div>
+                ) : (
+                  <div className="flex items-center">
+                    <Image src="/file.svg" width={40} height={40} alt="file" />
+                    <p className="">Drag and drop your .csv file here</p>
+                  </div>
+                )}
+              </>
             )}
           </div>
-          <div className="border basis-3/5">Description Div</div>
+          <div className="basis-3/5 py-8 px-6">Description Div</div>
         </div>
       ),
     },
@@ -39,6 +66,7 @@ function Import() {
         />
         <link rel="icon" href="/icon.svg" />
       </Head>
+
       {steps[currentStep].Component}
     </div>
   );
