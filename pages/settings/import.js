e77a1253c330d8f7bccd6bb4a923076175ebb5f5
Eric <84822642+erichen114@users.noreply.github.com>
2022-03-04 09:09:27 -0800

    added settings

--- pages/settings/import.js
+++ pages/settings/import.js
@@ -1,16 +1,28 @@
 import { useState, useCallback } from "react";
 import Head from "next/head";
 import DashboardLayout from "../../shared/components/UI/Layouts/DashboardLayout";
+import { useDropzone } from "react-dropzone";
 
 function Import() {
   const [currentStep, setCurrentStep] = useState(0);
+  const onDrop = useCallback((acceptedFiles) => {
+    //do something with files
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   const steps = [
     {
       Component: (
         <div className="container h-full flex flex-col py-24 px-12 justify-center">
-          
-          <div className="border basis-1/5">File Div</div>
+          <div className="flex justify-center items-center border basis-1/5" {...getRootProps()}>
+            <input {...getInputProps()} />
+            {isDragActive ? (
+              <p>Drop your file here!</p>
+            ) : (
+              <p className="">Drag and drop your .csv file here</p>
+            )}
+          </div>
           <div className="border basis-3/5">Description Div</div>
         </div>
       ),
